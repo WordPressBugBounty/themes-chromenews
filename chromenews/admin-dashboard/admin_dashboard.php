b6431ba3fd6b53ea2aa3d1b7bd300b144bd2c9d8
Sean Murphy <sean@iamseanmurphy.com>
2026-07-27 15:14:01 +0000

    6.2.5

--- chromenews/admin-dashboard/admin_dashboard.php
+++ chromenews/admin-dashboard/admin_dashboard.php
@@ -385,7 +385,7 @@ if (!class_exists('AF_themes_info')) {
       wp_enqueue_script(
         'aftheme-dashboard', // Handle.
         get_template_directory_uri() . '/admin-dashboard/dist/admin_dashboard.build.js',
-        array('react', 'react-dom', 'wp-api-fetch', 'wp-element'), // Dependencies, defined above.
+        array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), // Added 'wp-i18n'
         '1.0.0',
         true
       );
@@ -445,6 +445,27 @@ if (!class_exists('AF_themes_info')) {
         'activate_btn' => __('Activate', 'chromenews'),
         'installed_btn' => __('Activated', 'chromenews')
       ));
+
+      // 1. Core Dashboard React App
+      wp_set_script_translations(
+        'aftheme-dashboard',
+        'chromenews',
+        get_template_directory() . '/languages' // <-- Tells WP where to find source hashes
+      );
+
+      // 2. Plugin Installer
+      wp_set_script_translations(
+        'plugin-installer',
+        'chromenews',
+        get_template_directory() . '/languages'
+      );
+
+      // 3. TemplateSpare Installer
+      wp_set_script_translations(
+        'templatespare-installer',
+        'chromenews',
+        get_template_directory() . '/languages'
+      );
     }
 
     function chromenews_get_latest_changelog()
